11f9c7b513dcb0952ae387221ca6e10257729954
Thomas Francart <thomas.francart@sparna.fr>
2022-09-21 12:43:13 +0200

    Commit various files

---
+++ spring-boot/xls2rdf-rest/src/main/java/fr/sparna/rdf/xls2rdf/rest/Xls2rdfRestServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.sparna.rdf.xls2rdf.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xls2rdfRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Xls2rdfRestServiceApplication.class, args);
+	}
+
+}
---
+++ spring-boot/xls2rdf-rest/src/test/java/fr/sparna/rdf/xls2rdf/rest/Xls2rdfRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sparna.rdf.xls2rdf.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Xls2rdfRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
