dfd1722ec35062dfbedc6e98920f276261a6ac7b
Thomas Francart <thomas.francart@sparna.fr>
2023-01-29 10:42:08 +0100

    SimpleErrorController

---
+++ xls2rdf-rest/src/main/java/fr/sparna/rdf/xls2rdf/rest/SimpleErrorController.java
@@ -0,0 +1,56 @@
+package fr.sparna.rdf.xls2rdf.rest;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+
+@RestController
+@RequestMapping("/error")
+public class SimpleErrorController implements ErrorController {
+
+    private final ErrorAttributes errorAttributes;
+
+    @Autowired
+    public SimpleErrorController(ErrorAttributes errorAttributes) {
+        Assert.notNull(errorAttributes, "ErrorAttributes must not be null");
+        this.errorAttributes = errorAttributes;
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @RequestMapping
+    public Map<String, Object> error(HttpServletRequest aRequest) {
+        Map<String, Object> body = getErrorAttributes(aRequest, getTraceParameter(aRequest));
+        String trace = (String) body.get("trace");
+        if (trace != null) {
+            String[] lines = trace.split("\n\t");
+            body.put("trace", lines);
+        }
+        return body;
+    }
+
+    private boolean getTraceParameter(HttpServletRequest request) {
+        String parameter = request.getParameter("trace");
+        if (parameter == null) {
+            return false;
+        }
+        return !"false".equals(parameter.toLowerCase());
+    }
+
+    private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
+        WebRequest webRequest = new ServletWebRequest(request);
+        return this.errorAttributes.getErrorAttributes(webRequest, includeStackTrace);
+    }
+}
