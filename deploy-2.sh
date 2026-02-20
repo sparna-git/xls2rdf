5fc0790f04b6494a091850382c04fbd9ae2165a7
Thomas Francart <thomas.francart@sparna.fr>
2026-02-20 09:43:33 +0100

    xls2rdf-rest --> xls2rdf-web

--- deploy-2.sh
+++ deploy-2.sh
@@ -1,10 +1,10 @@
 # Transfer
-scp -i /home/thomas/.ssh/sparna-keypair-francfort.pem xls2rdf-rest/target/xls2rdf-rest-4.0.0.war ubuntu@92.243.27.145:xls2rdf-rest.war
+scp -i /home/thomas/.ssh/sparna-keypair-francfort.pem xls2rdf-web/target/xls2rdf-web-4.0.0.war ubuntu@92.243.27.145:xls2rdf-web.war
 
 # Redeploy
 ssh -i /home/thomas/.ssh/sparna-keypair-francfort.pem ubuntu@92.243.27.145 'sudo su -c "\
 service tomcat10 stop
-rm -rf /var/lib/tomcat10/webapps/rest.war
-rm -rf /var/lib/tomcat10/webapps/rest
-mv /home/ubuntu/xls2rdf-rest.war /var/lib/tomcat10/webapps/rest.war
+rm -rf /var/lib/tomcat10/webapps/web.war
+rm -rf /var/lib/tomcat10/webapps/web
+mv /home/ubuntu/xls2rdf-web.war /var/lib/tomcat10/webapps/web.war
 service tomcat10 start"'
